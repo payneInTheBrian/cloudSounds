51fc2706c11b9057aac1482851ef6ff8065d5100
iplaydrumsonmyguitar <gadomski.brian@gmail.com>
2022-11-30 14:10:01 -0600

    feed

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,7 +5,7 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname).toLowerCase();
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".mp4" && ext !== ".mov"  && ext !== ".mp3" && ext !== ".wav" && ext !== ".aiff") {
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".mp4" && ext !== ".mov"  && ext !== ".mp3" && ext !== ".wav" && ext !== ".aif") {
       cb(new Error("File type is not supported"), false);
       return;
     }
